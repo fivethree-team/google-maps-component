85a6c8a2150b06af4638b8fe848059f2356741ed
Marc Stammerjohann <marcstammerjohann@web.de>
2019-04-22 11:19:55 +0200

    chore(lib): add google-maps lib

---
+++ projects/google-maps/src/lib/google-maps.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-google-maps',
+  templateUrl: './google-maps.component.html',
+  styleUrls: ['./google-maps.component.css'],
+})
+export class GoogleMapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
